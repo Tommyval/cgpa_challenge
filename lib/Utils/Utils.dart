67f1457ee6e70fb9fd04b1c49c75860827cff8bd
Tommyval <oyintomiwao@gmail.com>
2022-09-08 22:53:00 +0100

    first commit

---
+++ lib/Utils/Utils.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class Utils {
+  static const Color maintheme = Color(0xffF5F5F5);
+  static const Color buttonColor = Color(0xffFF554D);
+  static const Color textColor = Color(0xff23693D);
+}
---
+++ lib/pages/start_page.dart
@@ -0,0 +1,50 @@
+import 'package:cgpa_challenge/pages/score_page.dart';
+import 'package:flutter/material.dart';
+
+import '../Utils/Utils.dart';
+
+class StartApp extends StatelessWidget {
+  const StartApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Future.delayed(const Duration(seconds: 10), () {
+      Navigator.of(context)
+          .push(MaterialPageRoute(builder: (context) => const ScorePage()));
+    });
+    return Scaffold(
+        backgroundColor: Utils.maintheme,
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [
+            Center(
+              child: Text(
+                'UCP',
+                style: TextStyle(
+                    fontSize: 24,
+                    color: Color(0xff23693D),
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+            Center(
+                child: Text(
+              'GPA \$ CGPA',
+              style: TextStyle(fontWeight: FontWeight.w500),
+            )),
+            SizedBox(
+              height: 10,
+            ),
+            Center(
+              child: SizedBox(
+                height: 40,
+                width: 40,
+                child: CircularProgressIndicator(
+                  strokeWidth: 6,
+                  valueColor: AlwaysStoppedAnimation<Color>(Color(0xff23693D)),
+                ),
+              ),
+            )
+          ],
+        ));
+  }
+}
